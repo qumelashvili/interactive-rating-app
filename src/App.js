9c628eb8ba38edf9bed2f4221f57727514f3ed67
Zao Kumelashvili <zaozao03@gmail.com>
2022-12-03 18:17:55 -0800

    deploy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import RatingComponent from './components/RatingComponent';
 import ThanksComponent from './components/ThanksComponent/ThanksComponent';
